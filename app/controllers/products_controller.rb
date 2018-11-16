632f428db1809ec46d18da2648b9b5590c3e9cc7
JonathanL1211 <Jonathanknight1211@gmail.com>
2018-11-16 13:42:06 +0800

    add: new fields for devise registration

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,6 @@
 class ProductsController < ApplicationController
 #ArticlesController is inheriting from a special class called ApplicationController
+  before_action :authenticate_user!
   def index
   end
 
