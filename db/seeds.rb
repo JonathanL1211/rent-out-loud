84df9b851d90d3e8082a9bbf447a561f7f732fc7
JonathanL1211 <Jonathanknight1211@gmail.com>
2018-11-16 23:05:41 +0800

    add: seed date

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+user1 = User.create!(email: "me@home.com", password: "watching the telly", name: "ME", contact_number: 90000010)
+user2 = User.create!(email: "ben@gmail.com", password: "teletubbies", name: "Ben Cho", contact_number: 90020280)
+
+product1 = Product.create(name: "organ attack", description: "a game where you destroy your opponent's organs", price: 5.50, rental_days: 7, meetup_location: "Singapore, Orchard Road", availability_status: true, user_id: 1)
+
