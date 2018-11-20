730500bfdf80c7a20c6050671e7d6be616b662e3
JonathanL1211 <Jonathanknight1211@gmail.com>
2018-11-20 22:58:37 +0800

    add: tested activemailer

---
+++ app/mailers/order_mailer.rb
@@ -0,0 +1,15 @@
+class OrderMailer < ApplicationMailer
+
+  # Subject can be set in your I18n file at config/locales/en.yml
+  # with the following lookup:
+  #
+  #   en.order_mailer.new_order.subject
+  #
+  def new_order(product)
+    @product = product
+    @user = User.find(6)
+
+    mail to: @user.email,
+         subject: "Email confirmation for orders"
+  end
+end
