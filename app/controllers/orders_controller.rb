795c5cb98884a17bfba15ac48e6850ab6e12e849
JonathanL1211 <Jonathanknight1211@gmail.com>
2018-11-21 21:19:18 +0800

    fix: calculation of the total price for an item

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -18,7 +18,7 @@ class OrdersController < ApplicationController
     @product = Product.find(params[:product_id])
     @order = Order.new(orders_params)
     @order.rental_days = (@order.end_date - @order.meetup_date).to_i
-    @order.individual_cost = ((@order.rental_days / 7) * @product.price).round(2)
+    @order.individual_cost = ((@order.rental_days.to_f / 7) * @product.price).round(2)
     @order.save
 
     # OrderMailer.new_order(@product).deliver_now
